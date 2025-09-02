776626df8807917450eb2e5e0f1bd6513b33040e
anonymous298 <tackletalha@gmail.com>
2025-09-02 18:42:45 +0500

    footer designed

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className= ' text-center text-white font-bold text-[17px] p-4'>
+      &copy; Connecto All Rights Reserved | Made With ❤️ By TALHA
+    </div>
+  )
+}
+
+export default Footer
