afbfcdc301546279127d8fddecfdf3bc2f500b07
anonymous298 <tackletalha@gmail.com>
2025-09-03 00:40:11 +0500

    login route created

---
+++ app/login/page.js
@@ -0,0 +1,65 @@
+"use client"
+
+import Image from "next/image";
+import { FaGoogle, FaGithub, FaFacebook, FaApple, FaMicrosoft } from "react-icons/fa";
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+    const {data : session} = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session) {
+            router.push('/dashboard')
+        }
+    }, [session])
+
+    return (
+        <div className="flex items-center justify-center min-h-screen gap-20 px-6 max-[800px]:flex-col-reverse ">
+            {/* Login Card */}
+            <div className="bg-white rounded-2xl shadow-lg p-8 w-[40%] max-w-md text-center max-[800px]:w-[70%]">
+                <h1 className="text-[27px] font-extrabold mb-6 text-gray-800">Login</h1>
+
+                <div className="flex flex-col gap-y-3">
+                    <button onClick={() => signIn('google')} className="flex items-center justify-center gap-2 w-full bg-red-500 text-white py-4 px-6 rounded-lg hover:bg-red-600 transition-all cursor-pointer">
+                        <FaGoogle className="text-lg" />
+                        Continue with Google
+                    </button>
+
+                    <button onClick={() => signIn('github')}  className="flex items-center justify-center gap-2 w-full bg-gray-800 text-white py-4 px-6 rounded-lg hover:bg-black transition-all cursor-pointer">
+                        <FaGithub className="text-lg" />
+                        Continue with GitHub
+                    </button>
+
+                    <button onClick={() => signIn('facebook')}  className="flex items-center justify-center gap-2 w-full bg-[#1877F2] text-white py-4 px-6 rounded-lg hover:bg-blue-700 transition-all cursor-pointer">
+                        <FaFacebook className="text-lg" />
+                        Continue with Facebook
+                    </button>
+
+                    <button onClick={() => signIn('apple')}  className="flex items-center justify-center gap-2 w-full bg-[#000000] text-white py-4 px-6 rounded-lg hover:bg-gray-800 transition-all cursor-pointer">
+                        <FaApple className="text-lg" />
+                        Continue with Apple
+                    </button>
+
+                    <button onClick={() => signIn('google')}  className="flex items-center justify-center gap-2 w-full bg-[#00A4EF] text-white py-4 px-6 rounded-lg hover:bg-[#3cc1ff] transition-all cursor-pointer">
+                        <FaMicrosoft className="text-lg" />
+                        Continue with Microsoft
+                    </button>
+                </div>
+            </div>
+
+            {/* Illustration */}
+            <div className="w-[40%] max-[800px]:w-[70%]">
+                <Image
+                    src="/vector.png"
+                    alt="Login Illustration"
+                    width={600}
+                    height={600}
+                    className="rounded-2xl object-contain"
+                />
+            </div>
+        </div>
+    );
+}
