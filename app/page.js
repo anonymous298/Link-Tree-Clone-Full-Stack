1f224ee7081e4e442bed67b69d522529457d8897
anonymous298 <tackletalha@gmail.com>
2025-09-03 03:31:06 +0500

    responsivness changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="container flex justify-center items-center min-h-screen max-[1030px]:mt-20 min-[1400px]:mt-20 max-[800px]:flex-col-reverse">
+      <div className="container flex justify-center items-center min-h-screen mt-10 max-[1030px]:mt-20 min-[1400px]:mt-20 max-[800px]:flex-col-reverse">
         <div className="w-[40%] flex flex-col justify-start gap-y-5 max-[800px]:w-[80%]">
           <h2 className="text-[#d2e823] font- font-extrabold text-[55px] w-[90%]">Everything you are. In one, simple link in bio.</h2>
           <p className="text-white font-sans text-[19px]">A simple way to share all your important links in one place — connect your audience to your website, social profiles, and content with a single link.</p>
