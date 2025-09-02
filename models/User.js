3d696026c2ecb67d0808804b80307c6d1db9f1ea
anonymous298 <tackletalha@gmail.com>
2025-09-03 03:07:21 +0500

    schema updated for profilepic

--- models/User.js
+++ models/User.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const linkSchema = new mongoose.Schema({
-  title: String,
-  url: String,
+    title: String,
+    url: String,
 });
 
 const userSchema = new mongoose.Schema({
-  fullname: { type: String },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  links: [linkSchema],
+    profilepic: { type: String },
+    fullname: { type: String },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    links: [linkSchema],
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
